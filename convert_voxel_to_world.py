80e7725b92a40a0903a13879c5b9f1a7db994e35
Haimasree Bhattacharya <haimasree.diag@gmail.com>
2019-04-24 17:02:32 +0200

    Refs #8483. Colin's suggested output json format implmented. Testing to be done.

--- convert_voxel_to_world.py
+++ convert_voxel_to_world.py
@@ -1,14 +1,11 @@
 import json
 import os
-import shutil
-
+import copy
 
 def convert_voxel_to_world(prep_folder, name, crop_rects_json_path,
                            output_path):
     if not os.path.exists(output_path):
         os.mkdir(output_path)
-    else:
-        shutil.rmtree(output_path)
     input_dict = {}
     preprocessing_info_file_name = os.path.join(prep_folder,
                                                 '{}_preprocessing_info.txt'.format(
@@ -40,9 +37,11 @@ def convert_voxel_to_world(prep_folder, name, crop_rects_json_path,
                 boundingbox_max[dim].append(float(min_and_max[1]))
 
     print("Original bounding box")
-    print(boundingbox_min)
-    print(boundingbox_max)
 
+    original_boundingbox_min = copy.deepcopy(boundingbox_min)
+    original_boundingbox_max = copy.deepcopy(boundingbox_max)
+    print(original_boundingbox_min)
+    print(original_boundingbox_max)
     for dim, value in input_dict['extendbox_origin'].iteritems():
         for index, min_coord in enumerate(
                 boundingbox_min[dim]):
@@ -95,12 +94,42 @@ def convert_voxel_to_world(prep_folder, name, crop_rects_json_path,
     output_string = output_string[:-2]
     output_string += ']'
     print(output_string)
-    with open(os.path.join(output_path, "converted_voxel_to_world.txt"),
-              "w+") as output:
-        output.write(output_string)
-    # [(74.0 115.0 398.5)  # 5, (-65.0 45.0 494.5) #4, (-53.0 169.0 267.5) #3, (-136.0 17.0 279.5) #2, (-156.0 53.0 263.5) #1]
-    # [(74.0 115.0 398.5) #5, (-65.0 45.0 494.5) #4, (-53.0 169.0 267.5) #3, (-136.0 17.0 279.5) #2, (-156.0 53.0 263.5) #1]
-    #
-    # # TODO: Cleanup
-    # # TODO: Output JSON File format -
-    # # rel_x, rel_y, wc_x box start, wc_x box end, center
+    # with open(os.path.join(output_path, "converted_voxel_to_world.txt"),
+    #           "w+") as output:
+    #     output.write(output_string)
+
+    # TODO: Cleanup
+    # Output the new json format
+    number_of_nodules = len(nodule_centers['x'])
+    output_json_dict = []
+    for i in range(0, number_of_nodules):
+        output_json_dict_per_nodule = {"world_x": [], "world_y": [],
+                                       "world_z": [], "rel_voxel_x": [],
+                                       "rel_voxel_y": [], "rel_voxel_z": []}
+        output_json_dict_per_nodule["world_x"].append(boundingbox_min['x'][i])
+        output_json_dict_per_nodule["world_x"].append(nodule_centers['x'][i])
+        output_json_dict_per_nodule["world_x"].append(boundingbox_max['x'][i])
+        output_json_dict_per_nodule["world_y"].append(boundingbox_min['y'][i])
+        output_json_dict_per_nodule["world_y"].append(nodule_centers['y'][i])
+        output_json_dict_per_nodule["world_y"].append(boundingbox_max['y'][i])
+        output_json_dict_per_nodule["world_z"].append(boundingbox_min['z'][i])
+        output_json_dict_per_nodule["world_z"].append(nodule_centers['z'][i])
+        output_json_dict_per_nodule["world_z"].append(boundingbox_max['z'][i])
+        output_json_dict_per_nodule["rel_voxel_x"].append(
+            original_boundingbox_min['x'][i])
+        output_json_dict_per_nodule["rel_voxel_x"].append(
+            original_boundingbox_max['x'][i])
+        output_json_dict_per_nodule["rel_voxel_y"].append(
+            original_boundingbox_min['y'][i])
+        output_json_dict_per_nodule["rel_voxel_y"].append(
+            original_boundingbox_max['y'][i])
+        output_json_dict_per_nodule["rel_voxel_z"].append(
+            original_boundingbox_min['z'][i])
+        output_json_dict_per_nodule["rel_voxel_z"].append(
+            original_boundingbox_max['z'][i])
+
+        output_json_dict.append(output_json_dict_per_nodule)
+    with open(os.path.join(output_path,
+                           "detected_nodules_in_world_and_voxel_coordinates.json"),
+              "w") as json_handle:
+        json.dump(output_json_dict, json_handle)
