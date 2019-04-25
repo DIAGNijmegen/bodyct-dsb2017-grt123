f09687c92206c1a9fdf00dccc968473acf2b3961
Haimasree Bhattacharya <haimasree.diag@gmail.com>
2019-04-25 21:17:35 +0200

    Refs #8483. Refactored fully.

--- convert_voxel_to_world.py
+++ convert_voxel_to_world.py
@@ -1,17 +1,12 @@
 import json
 import os
-import copy
 
 
 class ConvertVoxelToWorld(object):
     def __init__(self, prep_folder, series_uid, crop_rects_json_path,
                  output_path):
         self.__conversion_parameters = {}
-        self.__boundingbox_min = {'x': [], 'y': [], 'z': []}
-        self.__boundingbox_max = {'x': [], 'y': [], 'z': []}
-        self.__boundingbox_centers = {'x': [], 'y': [], 'z': []}
-        self.__original_boundingbox_min = {}
-        self.__original_boundingbox_max = {}
+        self.__coordinates = []
         self.__preprocessing_info_file = os.path.join(prep_folder,
                                                       '{}_preprocessing_info.txt'.format(
                                                           series_uid))
@@ -31,7 +26,7 @@ class ConvertVoxelToWorld(object):
                     self.__preprocessing_info_file))
         self.get_relative_voxel_coordinates()
         self.compute_cropped_voxel_coordinates()
-        self.compute_world_coordiantes()
+        self.compute_world_coordinates()
         self.output_json()
 
     def get_conversion_parameters(self):
@@ -60,120 +55,45 @@ class ConvertVoxelToWorld(object):
             try:
                 with open(self.__crop_rects_json_path) as f:
                     cropped_rects = json.load(f)
-                for key, items in cropped_rects.iteritems():
+                for items in cropped_rects.values():
                     for boundingbox in items:
+                        coordinate = {"world_x": [], "world_y": [],
+                                      "world_z": []}
                         for dim, min_and_max in boundingbox.iteritems():
-                            self.__boundingbox_min[dim].append(
-                                float(min_and_max[0]))
-                            self.__boundingbox_max[dim].append(
-                                float(min_and_max[1]))
+                            coordinate['rel_voxel_{}'.format(dim)] = min_and_max
+                        self.__coordinates.append(coordinate)
             except IOError:
                 print("Cannot read {}".format(self.__crop_rects_json_path))
         else:
             print("{} does not exist".format(
                 self.__crop_rects_json_path))  # TODO: Should have raised an exception
-        self.__original_boundingbox_min = copy.deepcopy(self.__boundingbox_min)
-        self.__original_boundingbox_max = copy.deepcopy(self.__boundingbox_max)
-        print("Original bounding box")
-        print(self.__original_boundingbox_min)
-        print(self.__original_boundingbox_max)
 
     def compute_cropped_voxel_coordinates(self):
-        for dim, value in self.__conversion_parameters[
-            'extendbox_origin'].iteritems():
-            for index, min_coord in enumerate(
-                    self.__boundingbox_min[dim]):
-                self.__boundingbox_min[dim][index] = min_coord * \
-                                                     self.__conversion_parameters[
-                                                         'cropped_grid_shape'][
-                                                         dim] + value
-        for dim, value in self.__conversion_parameters[
-            'extendbox_origin'].iteritems():
-            for index, max_coord in enumerate(
-                    self.__boundingbox_max[dim]):
-                self.__boundingbox_max[dim][index] = max_coord * \
-                                                     self.__conversion_parameters[
-                                                         'cropped_grid_shape'][
-                                                         dim] + value
+        for coord in self.__coordinates:
+            for dim in ['x', 'y', 'z']:
+                coord['world_{}'.format(dim)] = [
+                    c * self.__conversion_parameters['cropped_grid_shape'][
+                        '{}'.format(dim)] + \
+                    self.__conversion_parameters['extendbox_origin'][
+                        '{}'.format(dim)] for c in
+                    coord['rel_voxel_{}'.format(dim)]]
 
-    def compute_world_coordiantes(self):
-        for dim, value in self.__conversion_parameters[
-            'original_origin'].iteritems():
-            for index, min_coord in enumerate(
-                    self.__boundingbox_min[dim]):
-                self.__boundingbox_min[dim][index] = min_coord * \
-                                                     self.__conversion_parameters[
-                                                         'resampled_spacing'][
-                                                         dim] + value
-
-        for dim, value in self.__conversion_parameters[
-            'original_origin'].iteritems():
-            for index, max_coord in enumerate(
-                    self.__boundingbox_max[dim]):
-                self.__boundingbox_max[dim][index] = max_coord * \
-                                                     self.__conversion_parameters[
-                                                         'resampled_spacing'][
-                                                         dim] + value
-        for dim, nodule_min_bounding_box in self.__boundingbox_min.iteritems():
-            for index, min_coord in enumerate(nodule_min_bounding_box):
-                self.__boundingbox_centers[dim].append(
-                    min_coord + (self.__boundingbox_max[dim][
-                                     index] - min_coord) * 0.5)
+    def compute_world_coordinates(self):
+        for coord in self.__coordinates:
+            for dim in ['x', 'y', 'z']:
+                coord['world_{}'.format(dim)] = [
+                    c * self.__conversion_parameters['resampled_spacing'][
+                        '{}'.format(dim)] + \
+                    self.__conversion_parameters['original_origin'][
+                        '{}'.format(dim)] for c in
+                    coord['world_{}'.format(dim)]]
+                start_and_end = coord['world_{}'.format(dim)]
+                coord['world_{}'.format(dim)] = [start_and_end[0],
+                                                 sum(start_and_end) * 0.5,
+                                                 start_and_end[1]]
 
     def output_json(self):
-        # Compute input to the listString field of MeVisLab
-        counter = len(self.__boundingbox_centers['x'])
-        output_string = '['
-        for coord_x, coord_y, coord_z in zip(self.__boundingbox_centers['x'],
-                                             self.__boundingbox_centers['y'],
-                                             self.__boundingbox_centers['z']):
-            output_string += '({} {} {}) #{}, '.format(coord_x, coord_y,
-                                                       coord_z,
-                                                       counter)
-            counter = counter - 1
-        output_string = output_string[:-2]
-        output_string += ']'
-        print(output_string)
-
-        number_of_nodules = len(self.__boundingbox_centers['x'])
-        output_json_dict = []
-        for i in range(number_of_nodules):
-            output_json_dict_per_nodule = {"world_x": [], "world_y": [],
-                                           "world_z": [], "rel_voxel_x": [],
-                                           "rel_voxel_y": [], "rel_voxel_z": []}
-            output_json_dict_per_nodule["world_x"].append(
-                self.__boundingbox_min['x'][i])
-            output_json_dict_per_nodule["world_x"].append(
-                self.__boundingbox_centers['x'][i])
-            output_json_dict_per_nodule["world_x"].append(
-                self.__boundingbox_max['x'][i])
-            output_json_dict_per_nodule["world_y"].append(
-                self.__boundingbox_min['y'][i])
-            output_json_dict_per_nodule["world_y"].append(
-                self.__boundingbox_centers['y'][i])
-            output_json_dict_per_nodule["world_y"].append(
-                self.__boundingbox_max['y'][i])
-            output_json_dict_per_nodule["world_z"].append(
-                self.__boundingbox_min['z'][i])
-            output_json_dict_per_nodule["world_z"].append(
-                self.__boundingbox_centers['z'][i])
-            output_json_dict_per_nodule["world_z"].append(
-                self.__boundingbox_max['z'][i])
-            output_json_dict_per_nodule["rel_voxel_x"].append(
-                self.__original_boundingbox_min['x'][i])
-            output_json_dict_per_nodule["rel_voxel_x"].append(
-                self.__original_boundingbox_max['x'][i])
-            output_json_dict_per_nodule["rel_voxel_y"].append(
-                self.__original_boundingbox_min['y'][i])
-            output_json_dict_per_nodule["rel_voxel_y"].append(
-                self.__original_boundingbox_max['y'][i])
-            output_json_dict_per_nodule["rel_voxel_z"].append(
-                self.__original_boundingbox_min['z'][i])
-            output_json_dict_per_nodule["rel_voxel_z"].append(
-                self.__original_boundingbox_max['z'][i])
-
-            output_json_dict.append(output_json_dict_per_nodule)
         with open(os.path.join(self.__output_path,
                                "detected_nodules_in_world_and_voxel_coordinates.json"),
                   "w") as json_handle:
-            json.dump(output_json_dict, json_handle)
+            json.dump(self.__coordinates, json_handle)
